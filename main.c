4add92afacecbfde4bd01ca2897201f010e95c10
xylevy <82037387+xylevy@users.noreply.github.com>
2023-02-22 07:09:09 -0800

    Update

--- main.c
+++ main.c
@@ -9,13 +9,13 @@
  */
 int main(int ac, char **av)
 {
-	info_t info[] = {INFO_INIT};
+	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
-	asm("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r"(fd)
-			: "r"(fd));
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
@@ -37,10 +37,8 @@ int main(int ac, char **av)
 		}
 		info->readfd = fd;
 	}
-
 	populate_env_list(info);
 	read_history(info);
 	hsh(info, av);
-
 	return (EXIT_SUCCESS);
 }
